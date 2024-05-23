a52df8ba7f5a88da3d06a1c768eb4df12353242f
Jan Patrick Pett <jpatrickpett@googlemail.com>
2024-05-23 14:23:26 +0100

    setup CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ import pandas as pd
 
 @pytest.fixture
 def regulon_df():
-    df = pd.read_csv("test_data/ionocyte_regulons.csv", index_col=0)
+    df = pd.read_csv("tests/test_data/ionocyte_regulons.csv", index_col=0)
     return df
