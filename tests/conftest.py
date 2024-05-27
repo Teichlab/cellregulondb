30bf0d091906d3ba8d13fe15d3fc243052216a9b
Jan Patrick Pett <jpatrickpett@googlemail.com>
2024-05-27 18:12:04 +0100

    subsample test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,5 @@ import pandas as pd
 @pytest.fixture
 def regulon_df():
     df = pd.read_csv("tests/test_data/ionocyte_regulons.csv", index_col=0)
+    df = df.sample(n=1000, random_state=0)
     return df
